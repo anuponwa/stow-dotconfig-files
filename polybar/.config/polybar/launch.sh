a5cdc639fa1ad71bd5fbfe0aec019573331999b0
Anupong Wannakrairot <anuponwa@scg.com>
2025-06-08 20:39:05 +0700

    Add monitor resolution support for polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,5 +10,5 @@ while pgrep -u $UID -x polybar /dev/null; do sleep 1; done
 polybar laptop &
 
 # Check for DP-0 and HDMI-0 monitors
-xrandr -q | grep "DP-0 connected" && polybar dp0 &
-xrandr -q | grep "HDMI-0 connected" && polybar hdmi0 &
+xrandr -q | grep "DP-0 connected" | grep "1920" && polybar dp096 & || polybar dp0 &
+xrandr -q | grep "HDMI-0 connected" | grep "1920" && polybar hdmi096 & || polybar hdmi0 &
