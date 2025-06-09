0aa0e513b9bf91e19900567df9abb7b9ecc57f5c
Anupong Wannakrairot <anuponwa@scg.com>
2025-06-09 21:53:03 +0700

    Fix polybar launch script with monitor resolution

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,5 +10,18 @@ while pgrep -u $UID -x polybar /dev/null; do sleep 1; done
 polybar laptop &
 
 # Check for DP-0 and HDMI-0 monitors
-xrandr -q | grep "DP-0 connected" | grep "1920" && polybar dp096 & || polybar dp0 &
-xrandr -q | grep "HDMI-0 connected" | grep "1920" && polybar hdmi096 & || polybar hdmi0 &
+if xrandr -q | grep "DP-0 connected" | grep "1920"; then
+    polybar dp096 &
+else
+    polybar dp0 &
+fi
+
+
+if xrandr -q | grep "HDMI-0 connected" | grep "1920"; then
+    polybar hdmi096 &
+else
+    polybar hdmi0 &
+fi
+
+# xrandr -q | grep "DP-0 connected" | grep "1920" && polybar dp096 & || polybar dp0 &
+# xrandr -q | grep "HDMI-0 connected" | grep "1920" && polybar hdmi096 & || polybar hdmi0 &
